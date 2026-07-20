3d327c5233f9061ba23220102d29ec2a82bc7795
andy <tieuphu216@gmail.com>
2026-07-20 13:29:41 +0900

    update home title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import HomePage from "@/features/home-page";
 
 export const metadata: Metadata = {
   title: { absolute: "DevTools" },
+<<<<<<< HEAD
   description: "A collection of handy, client-side DevTools.",
+=======
+  description: "A collection of handy, client-side developer tools.",
+>>>>>>> 7cfe43d (update home title)
 };
 
 export default function Page() {
